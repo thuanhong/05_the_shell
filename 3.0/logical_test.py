2d8be69c3e6eff95ed73f6354d9b2809dc270079
Your Name <you@example.com>
2019-04-16 08:15:59 +0700

    fix bug: exit_status of recursive

--- 3.0/logical_test.py
+++ 3.0/logical_test.py
@@ -10,23 +10,25 @@ def check_nested_list(item):
 
 
 def recursive(command_list):
+    exit_status = 0
     for index, item in enumerate(command_list):
         if item not in [['&&'], ['||']]:
             if check_nested_list(item):
-                recursive(item)
+                exit_status = recursive(item)
             else:
                 exit_status = run(item).returncode
-                try:
-                    if not exit_status and command_list[index + 1] == ['||'] \
-                            or exit_status and command_list[index + 1] == ['&&']:
-                        break
-                except IndexError:
-                    pass
+            try:
+                if not exit_status and command_list[index + 1] == ['||'] \
+                        or exit_status and command_list[index + 1] == ['&&']:
+                    break
+            except IndexError:
+                pass
+    return exit_status
 
 
 def main():
     command_list = [['echo', 'hello A'],
-                    ['&&'],
+                    ['||'],
                     [['echo', 'hello B1'], ['||'], ['echo', 'hello B2']],
                     ['&&'],
                     [[['echo', 'hello C11'], ['&&'], ['echo', 'hello C12']], ['||'], ['echo', 'hello C2']]]
