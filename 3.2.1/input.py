5c8ec76e7138d708c338bf3b80213fbe83356025
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-04-18 14:30:11 +0700

    Remove splitting

--- 3.2.1/input.py
+++ 3.2.1/input.py
@@ -58,8 +58,6 @@ def handle_input(user_input, set_vars):
 
     # logical:
     user_input = split_logical_operator(user_input)
-    print(user_input)
-    # user_input = [item.split() for item in user_input if item]
-    # print('filter 8:', user_input)
+    print('filter 8:', user_input)
 
     return user_input
