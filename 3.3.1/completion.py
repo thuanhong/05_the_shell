a71e0548bffa9b99f883bf463347e789a344e7a5
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-04-20 18:52:12 +0700

    check pycode, add comments

--- 3.3.1/completion.py
+++ 3.3.1/completion.py
@@ -4,7 +4,11 @@ from readline import set_completer, parse_and_bind
 
 
 def get_variables():
+    """
+    Get all shell's variables
 
+    Return a list of shell's variables
+    """
     list_variables = []
     for key in environ:
         list_variables.append(key)
@@ -12,8 +16,15 @@ def get_variables():
 
 
 def find_executable_files(each_path, list_executable_files):
+    """
+    Search for executable files inside a given path
+
+    Return list of files name
+    """
+
     try:
         for filename in listdir(each_path):
+            # if filename is executable file inside $PATH
             if which(filename):
                 list_executable_files.append(filename)
     except FileNotFoundError:
@@ -23,15 +34,23 @@ def find_executable_files(each_path, list_executable_files):
 
 
 def find_paths():
+    """
+    Search for executable files inside $PATH
+
+    Return list of files name
+    """
+
     list_paths = []
     list_executable_files = ['cd', 'exit', 'export', 'unset', 'history']
     try:
+        # create a list of path from $PATH
         for path in environ["PATH"].split(":"):
             path += "/"
             list_paths.append(path)
     except Exception:
         pass
 
+    # search for files inside each path
     for each_path in list_paths:
         find_executable_files(each_path, list_executable_files)
 
@@ -39,9 +58,22 @@ def find_paths():
 
 
 def get_completer_function(text, state):
+    """
+    Search for invalid options
+
+    @param text: user's unfinished input
+
+    Return the corresponding option
+    """
+
+    # default list of options
     names = find_paths() + listdir('.') + get_variables()
+
+    # only current files and folder listed
     if not text or text == " ":
         names = listdir('.')
+
+    # search for valid options
     options = [word for word in names if
                word.startswith(text.replace(" ", ""))]
 
@@ -52,5 +84,8 @@ def get_completer_function(text, state):
 
 
 def auto_complete():
+    """
+    Auto complete the string while pressing tab
+    """
     set_completer(get_completer_function)
     parse_and_bind('tab: complete')
