237eb63a2117c4e8613e8daa1ef7eb6968ceceb2
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-04-21 14:25:21 +0700

    pycode checked, comments added

--- 4.0/signals_handling.py
+++ 4.0/signals_handling.py
@@ -2,16 +2,26 @@ from signal import signal, SIGINT, SIGQUIT, SIGTERM, SIGTSTP
 
 
 def call_signal(signal, frame):
+    """
+    Change exit code and do nothing else
+    """
     set_variables['exit_status'] = signal + 128
     pass
 
 
 def call_sigint(signal, frame):
+    """
+    Change exit code and raise KeyboardInterrupt exception
+    """
     set_variables['exit_status'] = signal + 128
     raise KeyboardInterrupt
 
 
 def control_signal():
+    """
+    Depend on user's action on keyboard
+    Run the corresponding function to handle the signal
+    """
     signal(SIGINT, call_sigint)
     signal(SIGQUIT, call_signal)
     signal(SIGTERM, call_signal)
