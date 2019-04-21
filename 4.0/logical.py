2769bbca45ce93d78152ca802dce65229a5e5fd0
vmtngan <48184625+vmtngan@users.noreply.github.com>
2019-04-21 13:55:23 +0700

    catch IndexError, refactor code

--- 4.0/logical.py
+++ 4.0/logical.py
@@ -11,63 +11,49 @@ def split_logical_operator(user_input):
         if user_input[index] in ['(', ')']:
             if user_input[index] == '(':
                 if braces:
-                    # 1.1.2
                     string += '('
-                    index += 1
-                    braces += 1
                 else:
-                    # 1.1.1
                     new_input.append(string)
                     string = '('
-                    index += 1
-                    braces += 1
+                braces += 1
             else:
                 braces -= 1
-                if braces:
-                    # 1.2.2
-                    string += user_input[index]
-                    index += 1
-                else:
-                    # 1.2.1
-                    string += user_input[index]
-                    new_input.append(string)
-                    string = ''
-                    index += 1
-        else:
-            if user_input[index] in ['|', '&'] and user_input[index] == user_input[index + 1]:
-                if braces:
-                    # 2.1.2
-                    string += user_input[index: index + 2]
-                    index += 2
-                else:
-                    # 2.1.1
-                    new_input.append(string)
-                    new_input.append(user_input[index: index + 2])
-                    string = ''
-                    index += 2
-            else:
-                # 2.2
                 string += user_input[index]
-                index += 1
+                if not braces:
+                    new_input.append(string)
+                    string = ''
+        else:
+            try:
+                if (user_input[index] in ['|', '&'] and
+                        user_input[index] == user_input[index + 1]):
+                    if braces:
+                        string += user_input[index: index + 2]
+                    else:
+                        new_input += [string, user_input[index: index + 2]]
+                        string = ''
+                    index += 1
+                else:
+                    string += user_input[index]
+            except IndexError:
+                pass
+        index += 1
     new_input.append(string)
     return [item.strip() for item in new_input if item.strip()]
 
 
 def run_logical_operator(command_list, set_vars):
     for index, item in enumerate(command_list):
-        if item not in ['&&', '||'] and not item.startswith("(") and not item.endswith(")"):
-            try:
-                run_command(item.split(), set_vars)
-            except Exception:
-                set_vars['exit_status'] = -1
+        if item.startswith('(') and item.endswith(')'):
+            command = (dirname(abspath(__file__)) + "/intek-sh.py "
+                       + item[1:-1] + " && exit $?")
+            run_command(command.split(), set_vars)
         elif item not in ['&&', '||']:
             try:
-                run_command((dirname(abspath(__file__)) + "/intek-sh.py " + item[1:-1] + " && exit $?").split(), set_vars)
-            except Exception:
-                set_vars['exit_status'] = -1
-        try:
-            if (not set_vars['exit_status'] and command_list[index + 1] == '||') \
-                    or (set_vars['exit_status'] and command_list[index + 1] == '&&'):
-                break
-        except IndexError:
-            pass
+                if ((set_vars['exit_status'] == 0 and
+                     command_list[index - 1] == '||')
+                    or (set_vars['exit_status'] != 0 and
+                        command_list[index - 1] == '&&')):
+                    continue
+            except IndexError:
+                pass
+            run_command(item.split(), set_vars)
