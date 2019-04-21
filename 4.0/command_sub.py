624d749300798a0a98e4e51853816dc76c26f7de
khagkhangg <48564724+khagkhangg@users.noreply.github.com>
2019-04-21 14:00:01 +0700

    Pycode checked, comments added

--- 4.0/command_sub.py
+++ 4.0/command_sub.py
@@ -4,26 +4,30 @@ from globbing import get_pathname_list
 
 
 def remove_quotes(substring):
-	return substring.group(0)[1:-1]
+	# return substring without the starting and ending character
+
+    return substring.group(0)[1:-1]
 
 
 def process_command(command):
     """
-    #XX
-    arg - type regex, dùng arg.group(x) để lấy ra string
-    string thường có dạng "$(abc)"
-    return string - string mới này sẽ được dùng để thay thế string command cũ trong substring
+    Turn string inside $() or `` into "command + arguments"
+    Then run that command
+    And return that command's output
     """
 
     try:
         if command.group(0):
             command = command.group(0)
+            # remove unused symbols
             if command.startswith("$("):
                 command = command[2:-1]
             elif command.startswith("`"):
                 command = command[1:-1]
+            # recursively search for nested command substution
             command = search_command_sub(command)
     except AttributeError:
+        # stop the recursion
         if command.startswith("$("):
             command = command[2:-1]
         elif command.startswith("`"):
@@ -31,45 +35,71 @@ def process_command(command):
 
     command = command.split()
     if len(command) > 1:
+        # create list of arguments
         arguments = command[1:]
         arguments = get_pathname_list(" ".join(arguments)).split()
     else:
+        # no argument
         arguments = [" "]
 
     command = command[0]
+    # get output
+    output = check_output([command] + arguments).decode("utf-8")
 
-    return check_output([command] + arguments).decode("utf-8").replace("\n", "")
+    # remove all line-break from output
+    return output.replace("\n", "")
 
 
 def search_command_sub(arg):
     """
-    arg - type regex, dùng arg.group(x) để lấy ra substring
-    substring thường có dạng "$(abc)"
-    return substring bị thay đổi
+    Search for command-subs nested inside quotes
+    Change them to usable strings
+
+    @param arg: type regex, arg.group(0) is a substring
+
+    Return substring that already changed
     """
+
     try:
+        # recursively find for deep-level nested:
         for number in [1, 3, 4]:
             if arg.group(number):
-                variable = sub(r"(?<!\\)\$\((?:(?!(?<!\\)\").)*(?<!\\)\)|(?<!\\)\`(?:(?!(?<!\\)\").)*(?<!\\)\`",
+                variable = sub(r"(?<!\\)\$\((?:(?!(?<!\\)\").)*(?<!\\)\)\
+                                 |(?<!\\)\`(?:(?!(?<!\\)\").)*(?<!\\)\`",
                                process_command, arg.group(number))
                 return variable
     except AttributeError:
-        variable = sub(r"(?<!\\)\$\((?:(?!(?<!\\)\").)*(?<!\\)\)|(?<!\\)\`(?:(?!(?<!\\)\").)*(?<!\\)\`",
+        # stop recursion
+        variable = sub(r"(?<!\\)\$\((?:(?!(?<!\\)\").)*(?<!\\)\)\
+                         |(?<!\\)\`(?:(?!(?<!\\)\").)*(?<!\\)\`",
                        process_command, arg)
         return variable
+
+    # return it-self if it's an invalid command-sub
     if arg.group(2):
-    	return arg.group(2)
+        return arg.group(2)
     elif arg.group(5):
         return arg.group(5)
 
 
 def command_sub(command):
     """
-    command - string
-    từng string trong list commands trong hàm main
-    return string đã bị thay đổi
+    Search for quotes, subshell, and valid command-subs
+    Change command-subs into usable strings
+
+    @param command: string, user's input
+
+    Return the whole string that already changed
     """
-    user_command = sub(r"((?<!\\)\"(?:(?!(?<!\\)\").)*(?<!\\)\")|((?<!\\)\'(?:(?!(?<!\\)\').)*\')|((?<!\\)\`(?:(?!(?<!\\)\`).)*(?<!\\)\`)|((?<!\\)\$\((?:(?!(?<!\\)\)).)*(?<!\\)\))|((?<!\\)\((?:(?!(?<!\\)\)).)*(?<!\\)\))",
-                   search_command_sub, command)
-    user_command = sub(r"((?<!\\)\"(?:(?!(?<!\\)\").)*(?<!\\)\")|((?<!\\)\'(?:(?!(?<!\\)\').)*\')", remove_quotes, user_command)
+
+    user_command = (sub(r"((?<!\\)\"(?:(?!(?<!\\)\").)*(?<!\\)\")\
+                          |((?<!\\)\'(?:(?!(?<!\\)\').)*\')\
+                          |((?<!\\)\`(?:(?!(?<!\\)\`).)*(?<!\\)\`)\
+                          |((?<!\\)\$\((?:(?!(?<!\\)\)).)*(?<!\\)\))\
+                          |((?<!\\)\((?:(?!(?<!\\)\)).)*(?<!\\)\))",
+                    search_command_sub, command))
+    # Find and remove all quotes
+    user_command = sub(r"((?<!\\)\"(?:(?!(?<!\\)\").)*(?<!\\)\")\
+                         |((?<!\\)\'(?:(?!(?<!\\)\').)*\')",
+                       remove_quotes, user_command)
     return user_command
--- 4.0/handle_backslash.py
+++ 4.0/handle_backslash.py
@@ -1,15 +1,32 @@
 def encode_backslash(user_input):
+    """
+    Turn double backslash into
+    a single backslash inside squares
+    """
+
     return user_input.replace("\\\\", "[\\]")
-    
+
 
 def decode_backslash(user_input):
+    """
+    Turn a single backslash inside squares
+    into a single backslash
+    """
+
     user_input.replace("[\\]", "\\")
     return user_input.replace("[\\]", "\\")
 
 
 def remove_backslash(user_input):
+    """
+    Remove all backslash that is used to escaped another character
+    """
+
     list_string = list(user_input)
-    index = 0 
+    index = 0
+
+    # check if it is an decoded backslash or not
+    # if false, remove it
     while index < len(list_string):
         if list_string[index] == "\\" and list_string[index - 1] != "[":
             list_string.pop(index)
